fb1bff8d28b96c56a663f7644f8508814aedbfe2
Eddie Soller <esoller@apexclearing.com>
2020-09-03 18:08:37 -0700

    refactor contentScript

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -9,13 +9,14 @@ chrome.storage.sync.get('starredUsers', data => {
 $(document).ready(() => {
     $("li.message").each((index, element) => {
         const postAuthor = $(element).attr("data-author");
+        const dateElement = $(element).find("div.postHeader > a.datePermalink");
         if (starredUsers.includes(postAuthor.toLowerCase())) {
-            $("<span class='cesIcon starredUserPost'>⭐</span>").insertBefore($(element).find("div.postHeader > a.datePermalink"));
+            $("<span class='cesIcon starredUserPost'>⭐</span>").insertBefore($(dateElement));
         }
 
         const manyLikes = $(element).find("span.LikeText > a.OverlayTrigger").get().length > 0;
         if (manyLikes) {
-            $("<span class='cesIcon manyLikesPost'>❤️</span>").insertBefore($(element).find("div.postHeader > a.datePermalink"));
+            $("<span class='cesIcon manyLikesPost'>❤️</span>").insertBefore($(dateElement));
         }
     });
 });
